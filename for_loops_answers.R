c88e934427393d0383caca4f914e57a964667f3a
schwarzm3283 <schwarz.mike@gmail.com>
2016-01-12 11:32:39 -0500

    answer

--- for_loops_answers.R
+++ for_loops_answers.R
@@ -1,3 +1,4 @@
+##for loop first anser
 x <- sample(1:100, 20);x
 
 
@@ -11,3 +12,32 @@ for(i in 1:length(x)) {
   return(x.min)
 }
 
+##For loop second answer
+v1 <- sort(sample(1:50, 10))
+v2 <- sort(sample(1:50, 10))
+
+merg.vec <- function(v1,v2) {
+  i <- 1
+  j <- 1
+  z <- rep(0, (length(v1) + length(v2)))
+  while (i <= length(v1) && j <= length(v2)) {
+    if (v1[i] < v2[j]) {
+      z[i+j-1] <- v1[i]
+      i <- i+1
+    }
+    else {
+      z[i+j-1] <- v2[j]
+      j <- j+1
+    }
+  }
+  if (j > length(v2)) {
+    z[(i+j-1):length(z)] <- v1[i:length(v1)]
+  } else if (i > length(v1)) {
+    z[(i+j-1):length(z)] <- v2[j:length(v2)]
+  } else {
+    cat('ERROR!\n')
+  }
+  return(z)
+}
+
+##for loop thrid answer
