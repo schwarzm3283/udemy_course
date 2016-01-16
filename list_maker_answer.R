e55bc200f1c457c50b6d4a4818fecf331935be56
schwarzm3283 <schwarz.mike@gmail.com>
2016-01-16 09:45:47 -0500

    answer

--- list_maker_answer.R
+++ list_maker_answer.R
@@ -17,10 +17,54 @@
 # input vectors. The input vectors can be any 
 # mode, but must be of same length  
 
+vec1 <- sample(letters, 10)
+vec2 <- sample(1:50, 10)
+vec3 <- sample(1:50, 20)
+
 df.maker <- function(...){
-  test <- list(...)
-  return(test)
-}
+  vec.list <- list(...)
+  len.check <- sapply(vec.list, length)
+  len.measure <- abs(len.check) - mean(len.check)
+  ven.length <- length(vec.list)
+  
+  vector.maker <- function(len=sample(2:9,1)) {
+    for (i in 1:len) {
+      x <- sample(1:3,1)
+      if (x==1){
+        rand.num <- sample(1:10,len.check[1], replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.num
+      }
+      else if (x==2) {
+        rand.alph <- sample(letters,len.check[1], replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.alph
+      }
+      else {
+        rand.bool <- sample(c(TRUE, FALSE),len.check[1], replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.bool
+      }
+    }
+    return(vec.list)
+  }
+  if (len.measure[1] != 0) {
+    stop("All vectors must be the same length")
+  }
+  else if (ven.length < 2) {
+      vec.list <- vector.maker()
+      names <- 1:length(vec.list)
+      df.result <- data.frame(vec.list)
+      colnames(df.result) <- names
+      return(df.result)
+    }
+    else {
+      names <- 1:length(vec.list)
+      df.result <- data.frame(vec.list)
+      colnames(df.result) <- names
+      return(df.result)
+    }
+  }
+
+
+
 
 ###############   LIST MAKER    ##########################  
 # list.maker(...) accepts a variable number of (optional) 
@@ -32,5 +76,50 @@ df.maker <- function(...){
 # Hint: Is OK to use the previous vector.maker() function 
 # in either dataframe.maker() or list.maker().  
 
-
+list.maker <- function(...,len=sample(3:10,1)){
+  vec.list <- list(...)
+  vector.maker <- function() {
+    for (i in 1:len) {
+      x <- sample(1:5,1)
+      if (x==1){
+        rand.num <- sample(1:10,sample(5:50,1), replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.num
+      }
+      else if (x==2) {
+        rand.alph <- sample(letters,sample(5:50,1), replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.alph
+      }
+      else if (x==3) {
+        row <- sample(5:10,1)
+        col <- sample(5:10,1)
+        tot <- (row * col)
+        y <- sample(1:100,tot, replace = TRUE)
+        rand.matrix <- matrix(y, nrow = row, ncol = col)
+        vec.list[[1+length(vec.list)]] <- rand.matrix
+      }
+      else if (x==4) {
+        row <- sample(5:10,1)
+        col <- sample(5:10,1)
+        tot <- (row * col)
+        y <- sample(1:100,tot, replace = TRUE)
+        rand.matrix <- matrix(y, nrow = row, ncol = col)
+        rand.df <- data.frame(rand.matrix)
+        vec.list[[1+length(vec.list)]] <- rand.df
+      }
+      else {
+        rand.bool <- sample(c(TRUE, FALSE),sample(5:50,1), replace=TRUE)
+        vec.list[[1+length(vec.list)]] <- rand.bool
+      }
+    }
+    return(vec.list)
+  }
+  if (length(vec.list) > 0) {
+    return(vec.list)
+  }
+  else {
+    vec.list <- vector.maker()
+    return(vec.list)
+    
+  }
+}
 
